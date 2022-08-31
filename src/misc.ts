fa9d133d1beadfec75e376b3ba6aba934f45718c
Brett <bretthoganpr@gmail.com>
2022-08-31 19:13:43 -0400

    Misc: added get random value of array

--- src/misc.ts
+++ src/misc.ts
@@ -4,3 +4,7 @@ export function GetAllowed(Table: boolean[][][], row: number, column: number) {
     const Ret = Table[row][column].map((e, i) => e ? i : undefined)
     return Ret.filter((e) => e)
 }
+
+export function GetRandomValue(arr: any[]) {
+    return arr[Math.floor(Math.random() * arr.length)]
+}
